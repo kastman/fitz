e0dd741dccabdf06b5cef0d6c70fea732133198c
hobs <hobs+github@totalgood.com>
2019-05-20 14:10:15 -0700

    fix imports within __init__.py

--- fitz/__init__.py
+++ fitz/__init__.py
@@ -1,7 +1,29 @@
-from frontend import *
-import tools
-import os.path as op
+# from fitz.frontend import *
+# import tools
+# import os.path as op
+from pkg_resources import DistributionNotFound, get_distribution
+import os
+from os.path import dirname, basename, isfile
+import glob
+import django
 
-with open(op.join(op.dirname(__file__), 'VERSION')) as version_file:
-    version = version_file.read().strip()
-__version__ = version
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as version_file:
+        version = version_file.read().strip()
+    __version__ = version
+    del version
+finally:
+    del get_distribution, DistributionNotFound
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imagecanopy.settings')
+django.setup()
+
+modules = glob.glob(os.path.join(dirname(__file__), '*.py'))
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+from . import *  # noqa
